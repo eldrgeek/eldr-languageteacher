0dcd95f1682e0d75160f14595af9ec571f7dbcd4
Mike Wolf <mw@mike-wolf.com>
2020-02-08 20:50:28 -0500

    Create state for language teacher

--- src/app/state.js
+++ src/app/state.js
@@ -2,16 +2,39 @@ console.log('loading state');
 export const state = {
   title: 'The application title',
   devState: {
-    stateAttributes: 'testValue,value1',
+    stateAttributes: 'fragments,fragm',
     restoreState: false,
-    saveState: true,
+    saveState: false,
     logDiags: {
-      save: false,
+      save: true,
       restore: true,
     },
   },
-
-  testValue: 'testing value',
-  value1: 'new value1',
-  logValues: ['a', 'b'],
+  fragments: [
+    {
+      time: 0,
+      source:
+        'Państwo Dursleyowie spod numeru czwartego przy Privet Drive mogli z dumą twierdzić, że są całkowicie normalni, chwała Bogu.',
+    },
+    {
+      time: 1,
+      source:
+        'Byli ostatnimi ludźmi, których można by posądzić o udział w czymś dziwnym lub tajemniczym, bo po prostu nie wierzyli w takie bzdury.',
+    },
+    {
+      time: 1.5,
+      source: 'Pan Dursley był dyrektorem firmy Grunnings produkującej świdry.',
+    },
+  ],
+  fragmentIndex: 0,
+  mediaURL: 'https://soundcloud.com/mike-wolf-443507667/harry-potter-part-1',
+  mediaTime: 0,
+  userPlay: true,
+  play: true,
+  currentFragment: ({ fragmentIndex, fragments }) => fragments[fragmentIndex],
+  nextFragmentTime: ({ fragmentIndex, fragments }) => {
+    const fragment = fragments[fragmentIndex + 1];
+    if (!fragment) return null;
+    return fragment.time;
+  },
 };
